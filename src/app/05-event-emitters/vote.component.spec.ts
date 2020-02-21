c41093de08c3abdc2c3d4f75cb97cd27ea6b09bf
Himanshu Kumar <himanshu95.2010@gmail.com>
2020-02-21 19:55:46 +0530

    working with event emitters

--- src/app/05-event-emitters/vote.component.spec.ts
+++ src/app/05-event-emitters/vote.component.spec.ts
@@ -7,6 +7,11 @@ describe('VoteComponent', () => {
     component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should raise voteChanged event when upVoted', () => {
+    let totalVotes = null;
+    component.voteChanged.subscribe( tv => totalVotes = tv);
+    component.upVote();
+    // expect(totalVotes).not.toBeNull();
+    expect(totalVotes).toBe(1);
   });
 });
