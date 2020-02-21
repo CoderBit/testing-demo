790cb54f23edb34038f10e979f29de3d0daf9304
Himanshu Kumar <himanshu95.2010@gmail.com>
2020-02-21 17:51:58 +0530

    Unit testing fundamentals

---
+++ src/app/01-fundamentals/compute.spec.ts
@@ -0,0 +1,13 @@
+import { compute } from './compute';
+
+describe('compute', () => {
+  it('should return 0 it input is negative', () => {
+    const result = compute(-1);
+    expect(result).toBe(0);
+  })
+
+  it('should increment input if it is positive', () => {
+    const result = compute(1);
+    expect(result).toBe(2);
+  })
+})
