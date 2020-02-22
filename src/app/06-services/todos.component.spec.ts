7ebb12b3424690d5b90149326f775da740f4a1ed
Himanshu Kumar <himanshu95.2010@gmail.com>
2020-02-22 21:34:38 +0530

    working with services

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -2,6 +2,8 @@ import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service'; 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/throw';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
@@ -13,7 +15,7 @@ describe('TodosComponent', () => {
   });
 
   it('should set todos property with the items returned from the server', () => {
-    let todos = [1, 2, 3]
+    let todos = [1, 2, 3];
     spyOn(service, 'getTodos').and.callFake(() => {
       return Observable.from([todos]);
     });
@@ -23,5 +25,38 @@ describe('TodosComponent', () => {
     // expect(component.todos.length).toBeGreaterThan(0);
     // expect(component.todos.length).toBe(3);
     expect(component.todos).toBe(todos);
+  });
+
+  it('call add method', () => {
+    let spy = spyOn(service, 'add').and.callFake(t => {
+      return Observable.empty();
+    });
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should add the new todo returned from the server', () => {
+    let todo = { id: 1 };
+    /*
+    let spy = spyOn(service, 'add').and.callFake(t => {
+      return Observable.from([ todo ]);
+    });
+    */
+    let spy = spyOn(service, 'add').and.returnValue( Observable.from([ todo ]));
+
+    component.add();
+
+    expect(component.todos.indexOf(todo)).toBeGreaterThan(-1);
+  });
+
+  it('should set message property if server returns error', () => {
+    let error = 'error from the server';
+    let spy = spyOn(service, 'add').and.returnValue( Observable.throw(error));
+
+    component.add();
+
+    expect(component.message).toBe(error);
   });
 });
